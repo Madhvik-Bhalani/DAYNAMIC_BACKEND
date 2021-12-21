bb13167075ad305fe9a104fd61147aacc507c5b3
madhvik <madhvikbhalani37@gmail.com>
2021-12-21 18:56:18 +0530

    add con/package

--- db/conn.js
+++ db/conn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const con = async () => {
     try {
         // await mongoose.connect(`${process.env.compass}`)
-        await mongoose.connect(`${process.env.atlas}`)
+        await mongoose.connect(`${process.env.ATLAS}`)
         console.log('connected successfully..');
     } catch (e) {
         console.log(`connection error ${e}`);
