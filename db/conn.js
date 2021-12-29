61979a338ec51cc4ba8037f9567cc7820df65663
madhvik <madhvikbhalani37@gmail.com>
2021-12-29 21:43:06 +0530

    before heroku

--- db/conn.js
+++ db/conn.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const con = async () => {
     try {
-        await mongoose.connect(`${process.env.compass}`)
-        // await mongoose.connect(`${process.env.ATLAS}`)
+        // await mongoose.connect(`${process.env.compass}`)
+        await mongoose.connect(`${phrocess.env.ATLAS}`)
         console.log('connected successfully..');
     } catch (e) {
         console.log(`connection error ${e}`);
