330d2b2461b0548476f6327c2aa4d8c76ce6dc47
madhvik <madhvikbhalani37@gmail.com>
2021-12-29 22:14:31 +0530

    ok545

--- db/conn.js
+++ db/conn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const con = async () => {
     try {
         // await mongoose.connect(`${process.env.compass}`)
-        await mongoose.connect(`${process.env.ATLAS}`)
+        await mongoose.connect(`${process.env.MONGODB_URI}`)
         console.log('connected successfully..');
     } catch (e) {
         console.log(`connection error ${e}`);
