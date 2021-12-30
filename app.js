6be13e8a4f6c88206b0651dd8e7dafa77a039e47
madhvik <madhvikbhalani37@gmail.com>
2021-12-30 21:38:45 +0530

    ok

--- app.js
+++ app.js
@@ -197,7 +197,7 @@ app.post("/changepass", async (req, res) => {
         })
 
         const mailoptions = {
-            from: "cecile.schmeler82@ethereal.email",
+            from: `${process.env.USER}`,
             to: mail,
             subject: "your otp",
             text: `${otp}\n keep it secure and don't share with anyone`
